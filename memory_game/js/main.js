ed4aea3dd5e37e5d1789e5c25d9e839f171fb88b
xiamjf <xiamjf@gmail.com>
2018-10-08 23:49:59 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,20 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
+var cardOne;
+cardOne = cards[0];
+cardsInPlay.push(cardOne);
+var cardTwo;
+cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+if (cardsInPlay.length === 2){
+	if (cardsInPlay[0]===cardsInPlay[1]) {
+		alert("You found a match!");
+	}
+	else {
+		alert("Sorry, try again.");
+	 }
+
+}
